8475e77b12f9f220b4e9b144185320caded832c8
TalesMGodois <tales.godois@gmail.com>
2015-06-07 18:10:39 -0300

    Primeiro commit

---
+++ src/br/com/ufg/sd/chat/User/IUser.java
@@ -0,0 +1,10 @@
+package br.com.ufg.sd.chat.User;
+
+/**
+ * Created by tales on 03/06/15.
+ */
+public interface IUser {
+    public boolean sendMessage(String msg);
+
+
+}
---
+++ src/br/com/ufg/sd/chat/User/User.java
@@ -0,0 +1,17 @@
+package br.com.ufg.sd.chat.User;
+
+/**
+ * Created by tales on 03/06/15.
+ */
+public class User {
+    private String nome;
+    private String matricula;
+    private String SobreNome;
+
+    public User(String name,String matricula){
+
+    }public User(String name,String matricula,String sobreNome){
+
+    }
+
+}
